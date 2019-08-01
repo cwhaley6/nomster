7f55a16a8bff5f6d00bfac699fc1cd4c892fecc6
cwhaley6 <christywhaley@gmail.com>
2019-08-01 02:15:50 +0000

    unique

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_30_195149) do
+ActiveRecord::Schema.define(version: 2019_08_01_021104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_07_30_195149) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_places_on_name", unique: true
   end
 
 end
